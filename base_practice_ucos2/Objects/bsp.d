70499e32c0490634499eebc679c2a336ccd5b288
DESKTOP-T5TDCQ8\Sam <swarrick6@gmail.com>
2025-01-30 17:36:27 -0500

    Stopped tracking unnecessary project files

--- base_practice_ucos2/Objects/bsp.d
+++
@@ -1,5 +0,0 @@
-./objects/bsp.o: bsp.c uc_ao.h ..\ucos2\Source\ucos_ii.h app_cfg.h \
-  os_cfg.h ..\ucos2\Ports\ARM-Cortex-M\ARMv6-M\ARM\os_cpu.h \
-  ..\ucos2\Source\os_trace.h qassert.h bsp.h \
-  ..\nucleo-c031c6\stm32c0xx.h ..\nucleo-c031c6\stm32c031xx.h \
-  ..\CMSIS\Include\core_cm0plus.h ..\nucleo-c031c6\system_stm32c0xx.h
--- base_practice_ucos2/Objects/uc_ao.d
+++
@@ -1,3 +0,0 @@
-./objects/uc_ao.o: uc_ao.c uc_ao.h ..\ucos2\Source\ucos_ii.h app_cfg.h \
-  os_cfg.h ..\ucos2\Ports\ARM-Cortex-M\ARMv6-M\ARM\os_cpu.h \
-  ..\ucos2\Source\os_trace.h qassert.h
